853c63c00f1cb8bc46ad1a52a2dbb54514a881d9
eharrington2019 <32171170+eharrington2019@users.noreply.github.com>
2017-10-03 21:11:26 -0400

    tried to print stuff

--- stoic.py
+++ stoic.py
@@ -1,10 +1,10 @@
 import random
-elements = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P",
+elements = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K",
+"Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",
+"Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe"]
 
 def main():
-    h = Element("h", 2)
-    o = Element("O", 1)
-
+    elements =
     print(h.symbol)
     print(o.symbol)
 
@@ -18,3 +18,14 @@ class Element:
   def __init__(self, symbol, count):
       self.symbol = symbol
       self.count = count
+
+printheader()
+selection = int(getUserSelection())
+def printHeader():
+    print("Stiochiometry")
+
+def getUserSelection()
+    left = input(Type left side)
+    print(left)
+
+main()
