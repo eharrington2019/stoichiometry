23d3b06bd42c446b16c9d249945fba5d8b9f319f
eharrington2019 <32171170+eharrington2019@users.noreply.github.com>
2017-10-12 10:53:20 -0400

    buildSide stuff

--- stoic.py
+++ stoic.py
@@ -7,14 +7,16 @@ symbol = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al"
 "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"]
 
 def main():
-  elements = [
-    Element("H", 1),
-    Element("Li", 2)
-    ]
-  printHeader()
-  selection1 = getUserSelection()
-  selection2 = getUserSelection2()
-  print(selection1, selection2)
+    printHeader
+    leftSide = buildSide()
+    rightSide = buildSide()
+    print(leftSide + "->" + rightSide)
+
+
+def buildSide():
+    if input(getAnswer) == "Stop":
+        print("Right Side")
+        getAnswer2
 
 class Compound:
   def __init__(self, element1, element2, count):
@@ -25,15 +27,33 @@ class Element:
       self.symbol = symbol
       self.count = count
 
+elements = [
+    Element("H", 1),
+    Element("Li", 2)
+    ]
+
+
+
 def printHeader():
     print("Stiochiometry")
 
-def getUserSelection():
-    leftside = input("Type left side: ")
-    return(leftside)
 
-def getUserSelection2():
-    rightside = input("Type right side: ")
-    return(rightside)
+def getAnswer(questionString, confirmList):
+    print("Left Side")
+    while(True):
+        answer = input(questionString)
+        if answer in confirmList:
+            return answer
+        elif answer == "Stop" or answer == "stop":
+            return answer
+
+def getAnswer2(questionString, confirmList):
+    print("Right Side")
+    while(True):
+        answer = input(questionString)
+        if answer in confirmList:
+            return answer
+        elif answer == "Stop" or answer == "stop":
+            return answer
 
 main()
